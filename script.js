fb43a3ccdf6a7f3aa4d3c233582a07b572ab77cd
Siddiq Ahmad Jafri <2k24.csai1c.2411969@gmail.com>
2026-03-15 01:24:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,62 @@
-let cart = [];
+function getCart(){
 
-function addCart(product){
+return JSON.parse(localStorage.getItem("cart")) || [];
 
-cart.push(product);
+}
 
-alert(product + " added to cart");
+function saveCart(cart){
 
-console.log(cart);
+localStorage.setItem("cart", JSON.stringify(cart));
+
+}
+
+function addToCart(name, price){
+
+let cart = getCart();
+
+cart.push({name,price});
+
+saveCart(cart);
+
+alert(name + " added to cart");
+
+}
+
+function displayCart(){
+
+let cart = getCart();
+
+let container = document.getElementById("cartItems");
+
+let total = 0;
+
+container.innerHTML="";
+
+cart.forEach((item,index)=>{
+
+total += item.price;
+
+container.innerHTML += `
+<div class="cart-item">
+${item.name} - ₹${item.price}
+<button onclick="removeItem(${index})">Remove</button>
+</div>
+`;
+
+});
+
+document.getElementById("total").innerText = "Total: ₹" + total;
+
+}
+
+function removeItem(index){
+
+let cart = getCart();
+
+cart.splice(index,1);
+
+saveCart(cart);
+
+displayCart();
 
 }
